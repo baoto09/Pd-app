f240b356b468a9b16570fc94f751cd55b5554062
baoto09 <baotao070805@gmail.com>
2025-06-30 18:29:59 +0700

    Update pd_streamlit_app.py

--- pd_streamlit_app.py
+++ pd_streamlit_app.py
@@ -46,6 +46,7 @@ if uploaded_file:
             st.error("❌ None matching batteries.")
         else:
             # Chuẩn bị bảng
+            st.info("✅ Appropriate batteries:")
             result_df = model_phu_hop.reset_index()
             result_df.columns = ["Batteries", "Power (W)"]
             result_df["Power (W)"] = result_df["Power (W)"].apply(lambda x: f"{int(x):,}".replace(",", "."))
