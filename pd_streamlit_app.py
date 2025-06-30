0e9993e50342c4e8033b11c0ad4455eefc07f997
baoto09 <baotao070805@gmail.com>
2025-06-30 16:34:48 +0700

    Update pd_streamlit_app.py

--- pd_streamlit_app.py
+++ pd_streamlit_app.py
@@ -46,7 +46,7 @@ if uploaded_file:
             st.error("❌ None batteries matched with requirements.")
         else:
             # Tạo bảng kết quả có STT
-            st.info("✅ Appropriate batteries:")
+            with st.info("✅ Appropriate batteries:")
             result_df = model_phu_hop.reset_index()
             result_df.columns = ["Batteries", "Power (W)"]
             result_df["Power (W)"] = result_df["Power (W)"].apply(lambda x: f"{int(x):,}".replace(",", "."))
@@ -58,9 +58,7 @@ if uploaded_file:
                 {"selector": "td", "props": [("text-align", "center")]}
             ]).hide(axis="index")
 
-            # Hiển thị bảng trong khung xanh biển
-            with st.info("✅ Appropriate batteries:", icon="✅"):
-                st.markdown(styled_table.to_html(), unsafe_allow_html=True)
+
     except Exception as e:
         st.error(f"⚠️ Lỗi: {e}")
 else:
